1ad3cfdfef57b123cc68c590c04728f4ad3cd133
David Fink <dfink@akamai.com>
2023-05-29 17:55:12 +0300

    Initial code from ChatGPT

---
+++ app.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let buttonStatus = false;
+let startTime = null;
+let currentTurn = null;
+
+// WebSocket connection handler
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    const { action, name } = JSON.parse(message);
+
+    if (action === 'toggleButton') {
+      if (currentTurn === name) {
+        toggleButton();
+      }
+    } else if (action === 'requestTurn') {
+      requestTurn(name, ws);
+    }
+  });
+});
+
+function toggleButton() {
+  buttonStatus = !buttonStatus;
+
+  if (buttonStatus && !startTime) {
+    startTime = Date.now();
+  } else if (!buttonStatus) {
+    startTime = null;
+  }
+
+  broadcastStatus();
+}
+
+function requestTurn(name, ws) {
+  if (currentTurn === null) {
+    currentTurn = name;
+    ws.send(JSON.stringify({ status: 'success', turn: true }));
+    broadcastStatus();
+  } else {
+    ws.send(JSON.stringify({ status: 'success', turn: false }));
+  }
+}
+
+function broadcastStatus() {
+  const elapsedTime = buttonStatus ? Date.now() - startTime : 0;
+  const seconds = Math.floor(elapsedTime / 1000);
+
+  const data = {
+    buttonStatus,
+    elapsedTime: seconds,
+    currentTurn: currentTurn || "No one's turn",
+  };
+
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+}
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: 'Please provide a name.' });
+  }
+
+  res.redirect(`/game?name=${encodeURIComponent(name)}`);
+});
+
+// Serve static files
+app.use(express.static(__dirname));
+
+server.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
